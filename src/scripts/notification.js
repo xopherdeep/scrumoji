ae109a4633dae4df1baf2032dc764d35814ffe8b
Oliver Schrenk <oliver.schrenk@gmail.com>
2018-08-26 16:40:55 +0200

    Make card clickable

--- src/scripts/notification.js
+++ src/scripts/notification.js
@@ -1,1 +1,4 @@
-!function(){var a=document.getElementById("scrumoji-list");a.addEventListener("click",function(b){if(b.target&&b.target.matches(".scrumoji")){b.target.classList.add("active"),b.target.classList.remove("active");var c=new NotificationFx({message:"<p>Hey! scrumoji copied to the clipboard 😜</p>",layout:"growl",effect:"scale",type:"notice",onClose:function(){a.disabled=!1}});c.show(),this.disabled=!0}})}();
+!function(){var a=document.getElementById("scrumoji-list");a.addEventListener("click",function(b){console.log("hey", b.target);
+  if(b.target.closest(".scrumoji").contains(b.target))
+  {console.log("match");b.target.classList.add("active"),b.target.classList.remove("active");var c=new NotificationFx({message:"<p>Hey! scrumoji copied to the clipboard 😜</p>",layout:"growl",effect:"scale",type:"notice",onClose:function(){a.disabled=!1}});c.show(),this.disabled=!0}}
+)}();
